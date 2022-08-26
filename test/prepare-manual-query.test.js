71ff1c9dfdb8c5feeef41d383e8b9e73aa02f4b5
Scott Howell <scctttt26@gmail.com>
2022-08-26 10:23:25 +0100

    Corrected test

--- test/prepare-manual-query.test.js
+++ test/prepare-manual-query.test.js
@@ -1,16 +1,15 @@
-var prepareManualQuery = require('../lib/prepare-manual-query'), assert = require('assert')
+var prepareManualQuery = require('../lib/prepare-manual-query')
+  , assert = require('assert')
 
 describe('prepareManualQuery()', function () {
 
   it('should limit the number of id\'s queried by the limit specified', function () {
-    var list = prepareManualQuery(
+    var q = prepareManualQuery(
       [ { _id: 0, shortTitle: 'James' }
-      , { _id: 1, shortTitle: 'Robert' }
-      ]
-    , [ { itemId: 1, overrides: { shortTitle: 'Bob' } }
-      , { itemId: 0, overrides: { shortTitle: 'Jim' } }
-      ]), limit = 2
-    list.slice(0, limit)
-    assert.deepEqual(list, [ { _id: 0, shortTitle: 'Jim' }, { _id: 1, shortTitle: 'Bob' } ])
+        , { _id: 1, shortTitle: 'Robert' }
+      ],
+      1
+    )
+    assert.deepEqual(q.query._id.$in, [0])
   })
 })
